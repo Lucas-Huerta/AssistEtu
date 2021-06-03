2be26daf319d62931ae62fb83b53583190c3ef63
Lucas <huertalucas13@gmail.com>
2021-06-03 18:23:23 +0200

    commit Mentions légales

---
+++ MentionsLegales.vue.js
@@ -0,0 +1,256 @@
+var Mentions = Vue.component('Mentions',{
+    template:`
+    
+    <div class="container-fluid">
+        <div class="col-12">
+            <div class="row">
+                <h1 class="texteMentions">
+                    Mention légales
+                </h1>                
+            </div>
+            
+            <div class="row">
+                <p>
+                    Merci de lire avec attention les différentes modalités d’utilisation du présent site avant 
+                    d’y parcourir ses pages. En vous connectant sur ce site, vous acceptez sans réserves les présentes modalités.
+                    Aussi, conformément à l’article n°6 de la Loi n°2004-575 du 21 Juin 2004 pour la confiance dans 
+                    l’économie numérique, les responsables du présent site internet http://assistetu.lucashuerta.fr  sont :
+                
+                </p>
+            </div>
+            
+            <div class="row">
+                <div class="col-6">
+                    <h3 class="texteMentions">
+                        Éditeurs du site                
+                    </h3>            
+                </div>
+                
+                <div class="col-6">
+                    <h3 class="texteMentions">
+                        Hébergement               
+                    </h3>            
+                </div>
+                
+                
+            </div>
+            
+            <div class="row">
+            
+            <div class="col-6">
+                    <p>
+                        Lucas Huerta, Robin Blondeau & Baptiste Rognon
+                        <br>
+                        4 place Tharradin, 25200 Montbéliard
+                        <br>
+                        Téléphone : 03 81 99 47 34
+                        <br>
+                        Email : contact@assistEtu.fr
+                        <br>
+                        Site Web : http://assistetu.lucashuerta.fr             
+                    </p>            
+                </div>
+                
+                <div class="col-6">
+                    <p>
+                        Société OVH
+                        <br>
+                        2 rue Kellermann, 59100 Roubaix
+                        v
+                        Site web : www.ovh.com            
+                    </p>            
+                </div>
+                
+                
+            </div>
+            
+            <div class="row">
+                <div class="col-12">
+                    <h3 class="texteMentions">
+                        Conditions d'utilisation
+                    </h3>                
+                </div>            
+            </div>
+            
+            <div class="row">
+                <div class="col-12">
+                    <p>
+                        Ce site (http://assistetu.lucashuerta.fr ) est proposé en différents langages web (HTML, HTML5, Javascript, CSS, etc…) pour un meilleur confort d'utilisation et un graphisme plus agréable, nous vous recommandons de recourir à des navigateurs modernes comme Internet explorer, Safari, Firefox, Google Chrome, etc…
+                        Nous mettons en œuvre tous les moyens dont nous disposons, pour assurer une information fiable et une mise à jour fiable de ce site internet.
+
+                        Toutefois, des erreurs ou omissions peuvent survenir. L'internaute devra donc s'assurer de l'exactitude des informations auprès de , et signaler toutes modifications du site qu'il jugerait utile. Nous ne sommes en aucun cas responsable de l'utilisation faite de ces informations, et de tout préjudice direct ou indirect pouvant en découler.
+                    </p>
+                
+                </div>
+            
+            </div>
+            
+            <div class="row">
+                <div class="col-6">
+                    <h3 class="texteMentions">
+                        Cookies              
+                    </h3>            
+                </div>
+                
+                <div class="col-6">
+                    <h3 class="texteMentions">
+                        Liens hypertextes               
+                    </h3>            
+                </div>
+            </div>
+            
+            <div class="row">
+                <div class="col-6">
+                    <p>
+                        Le site http://assistetu.lucashuerta.fr  peut-être amené à vous demander l’acceptation des cookies pour des besoins de statistiques et d'affichage.
+
+                        Un cookie est une information déposée sur votre disque dur par le serveur du site que vous visitez.
+
+                        Il contient plusieurs données qui sont stockées sur votre ordinateur dans un simple fichier texte auquel un serveur accède pour lire et enregistrer des informations .
+
+                        Certaines parties de ce site ne peuvent être fonctionnelles sans l’acceptation de cookies.            
+                    </p>            
+                </div>
+                
+                <div class="col-6">
+                    <p>
+                        Ce site peut contenir des liens vers d’autres ressources & d'autres sites web disponibles sur Internet.
+
+                        Nous ne disposons d'aucun moyen pour contrôler les sites en connexion avec ses sites internet.
+
+                        Nous ne pouvons être tenus pour responsables de tout dommage, de quelque nature que ce soit, résultant du contenu de ces sites ou sources externes, et notamment des informations, produits ou services qu’ils proposent, ou de tout usage qui peut être fait de ces éléments.            
+                    </p>            
+                </div>
+            </div>
+            
+            <div class="row">
+                <div class="col-12">
+                    <h3 class="texteMentions">
+                        Services fournis
+                    </h3>                
+                </div>            
+            </div>
+            
+            <div class="row">
+                <div class="col-12">
+                    <p>
+                        L'ensemble des activités de la société ainsi que ses informations sont présentés sur notre site http://assistetu.lucashuerta.fr.      
+                        Nous nous efforçons de fournir sur le site http://assistetu.lucashuerta.fr des informations aussi précises que possible. les renseignements figurant sur le site http://assistetu.lucashuerta.fr ne sont pas exhaustifs et les photos non contractuelles. Ils sont donnés sous réserve de modifications ayant été apportées depuis leur mise en ligne. Par ailleurs, toutes les informations indiquées sur le site http://assistetu.lucashuerta.fr sont données à titre indicatif, et sont susceptibles de changer ou d’évoluer sans préavis.                   
+                    </p>               
+                </div>           
+            </div>       
+            
+            <div class="row">
+                <div class="col-12">
+                    <h3 class="texteMentions">
+                        Limitation contractuelle sur les données
+                    </h3>                
+                </div>            
+            </div>
+            
+            <div class="row">
+                <div class="col-12">
+                    <p>
+                        Les informations contenues sur ce site sont aussi précises que possible et le site remis à jour à différentes périodes de l’année, mais peuvent toutefois contenir des inexactitudes ou des omissions.
+    
+                        Si vous constatez une lacune, erreur ou ce qui paraît être un dysfonctionnement, merci de bien vouloir le signaler par email, à l’adresse contact@assistEtu.fr, en décrivant le problème de la manière la plus précise possible (page posant problème, type d’ordinateur et de navigateur utilisé, …).
+                        
+                        Tout contenu téléchargé se fait aux risques et périls de l'utilisateur et sous sa seule responsabilité. En conséquence, nous ne saurions être tenu responsables d'un quelconque dommage subi par l'ordinateur de l'utilisateur ou d'une quelconque perte de données consécutives au téléchargement.
+                        
+                        De plus, l’utilisateur du site s’engage à accéder au site en utilisant un matériel récent, ne contenant pas de virus et avec un navigateur de dernière génération mis-à-jour Les liens hypertextes mis en place dans le cadre du présent site internet en direction d'autres ressources présentes sur le réseau Internet ne sauraient engager la responsabilité des responsables du site Assist'Etu
+                    </p>               
+                </div>           
+            </div>       
+            
+            
+             <div class="row">
+                <div class="col-12">
+                    <h3 class="texteMentions">
+                        Propriété intellectuelle
+                    </h3>                
+                </div>            
+            </div>
+            
+            <div class="row">
+                <div class="col-12">
+                    <p >
+                        Tout le contenu du présent sur le site http://assistetu.lucashuerta.fr, incluant, de façon non limitative, les graphismes, images, textes, vidéos, animations, sons, logos, gifs et icônes ainsi que leur mise en forme sont la propriété exclusive de la société à l'exception des marques, logos ou contenus appartenant à d'autres sociétés partenaires ou auteurs.
+                        
+                        Toute reproduction, distribution, modification, adaptation, retransmission ou publication, même partielle, de ces différents éléments est strictement interdite sans notre accord.
+                        
+                        Cette représentation ou reproduction, par quelque procédé que ce soit, constitue une contrefaçon sanctionnée par les articles L.335-2 et suivants du Code de la propriété intellectuelle.
+                        
+                        Le non-respect de cette interdiction constitue une contrefaçon pouvant engager la responsabilité civile et pénale du contrefacteur. En outre, les propriétaires des Contenus copiés pourraient intenter une action en justice à votre encontre.
+                    </p>               
+                </div>           
+            </div>  
+            
+             <div class="row">
+                <div class="col-6">
+                    <h3 class="texteMentions">
+                        Déclaration à la CNIL              
+                    </h3>            
+                </div>
+                
+                <div class="col-6">
+                    <h3 class="texteMentions">
+                        Litiges               
+                    </h3>            
+                </div>
+            </div>
+            
+            <div class="row">
+                <div class="col-6">
+                    <p>
+                        Conformément à la loi 78-17 du 6 janvier 1978 (modifiée par la loi 2004-801 du 6 août 2004 relative à la protection des personnes physiques à l'égard des traitements de données à caractère personnel) relative à l'informatique, aux fichiers et aux libertés, ce site n'a pas fait l'objet d'une déclaration auprès de la Commission nationale de l'informatique et des libertés (www.cnil.fr).         
+                    </p>            
+                </div>
+                
+                <div class="col-6">
+                    <p>
+                        Les présentes conditions du site http://assistetu.lucashuerta.fr sont régies par les lois françaises et toute contestation ou litiges qui pourraient naître de l'interprétation ou de l'exécution de celles-ci seront de la compétence exclusive des tribunaux dont dépend le siège social de la société. La langue de référence, pour le règlement de contentieux éventuels, est le français.         
+                    </p>            
+                </div>
+            </div>
+            
+             <div class="row">
+                <div class="col-12">
+                    <h3 class="texteMentions">
+                        Données personnelles
+                    </h3>                
+                </div>            
+            </div>
+            
+            <div class="row">
+                <div class="col-12">
+                    <p>
+                         De manière générale, vous n’êtes pas tenu de nous communiquer vos données personnelles lorsque vous visitez notre site Internet http://assistetu.lucashuerta.fr.
+                        
+                        Cependant, ce principe comporte certaines exceptions. En effet, pour certains services proposés par notre site, vous pouvez être amenés à nous communiquer certaines données telles que : votre nom, votre âge, votre localisation, votre adresse électronique etc. Tel est le cas lorsque vous remplissez le formulaire qui vous est proposé en ligne, dans la rubrique « À propos ».
+                        
+                        Dans tous les cas, vous pouvez refuser de fournir vos données personnelles. Dans ce cas, vous ne pourrez pas utiliser les services du site.
+                        
+                        Enfin, nous pouvons collecter de manière automatique certaines informations vous concernant lors d’une simple navigation sur notre site Internet, notamment : des informations concernant l’utilisation de notre site, comme les zones que vous visitez et les services auxquels vous accédez, votre adresse IP, le type de votre navigateur, vos temps d'accès.
+                        
+                        De telles informations sont utilisées exclusivement à des fins de statistiques internes, de manière à améliorer la qualité des services qui vous sont proposés. Les bases de données sont protégées par les dispositions de la loi du 1er juillet 1998 transposant la directive 96/9 du 11 mars 1996 relative à la protection juridique des bases de données.
+                    </p>               
+                </div>           
+            </div>  
+                 
+        </div>
+        
+          
+    </div>
+    
+            `,
+    data(){
+        return{
+        }
+    },
+    mounted(){
+
+    },
+    methods:{
+
+    }
+})
